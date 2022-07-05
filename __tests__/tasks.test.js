82d021cce43d86d96638125b2f83f36ae01f5b5b
Alex <box-570@yandex.ru>
2022-07-05 16:07:09 +0300

    fix tests

--- __tests__/tasks.test.js
+++ __tests__/tasks.test.js
@@ -1,20 +1,20 @@
 import fastify from 'fastify';
 import init from '../server/plugin.js';
-import { getTestData, prepareData } from './helpers/index.js';
+import { prepareData } from './helpers/index.js';
 
 // @ts-ignore
 
 describe('test tasks CRUD', () => {
   let app;
   let knex;
-  let models;
-  const testData = getTestData();
+  // let models;
+  // const testData = getTestData();
 
   beforeAll(async () => {
     app = fastify();
     await init(app);
     knex = app.objection.knex;
-    models = app.objection.models;
+    // models = app.objection.models;
 
     // TODO: пока один раз перед тестами
     // тесты не должны зависеть друг от друга
--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -7,5 +7,38 @@ export default (app) => {
 
       reply.render('tasks/index', { tasks });
       return reply;
+    })
+    .get('/tasks', { name: 'newTask' }, async (req, reply) => {
+      const task = new app.objection.models.task();
+      reply.render('tasks/new', { task });
+    })
+    .get('/tasks/:id', async (req, reply) => {
+      const { id } = req.params;
+      const task = await app.objection.models.task.query().findById(id);
+
+      reply.render('tasks/task', { task });
+    })
+    .get('/tasks/:id/edit', { name: 'editTask' }, async (req, reply) => {
+      const { id } = req.params;
+      const task = await app.objection.models.task.query().findById(id);
+
+      reply.render('tasks/task', { task });
+    })
+
+    .post('/tasks/new', async (req, reply) => {
+      const task = new app.objection.models.task();
+      task.$set(req.body.data);
+
+      try {
+        const validTask = await app.objection.models.task.fromJson(req.body.data);
+        await app.objection.models.task.query().insert(validTask);
+        // req.flash('info', i18next.t('flash.users.create.success'));
+        reply.redirect(app.reverse('root'));
+      } catch ({ data }) {
+        // req.flash('error', i18next.t('flash.users.create.error'));
+        reply.render('tasks/new', { task, errors: data });
+      }
+
+      return reply;
     });
 };
