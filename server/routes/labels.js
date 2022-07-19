228b02adcdbcc21aa443793534573c5d8626f951
Alex <box-570@yandex.ru>
2022-07-19 16:37:24 +0300

    add fix labels temp

--- server/routes/labels.js
+++ server/routes/labels.js
@@ -16,4 +16,20 @@ export default async (app) => {
       reply.render('labels/new');
       return reply;
     })
+    .post('/users', async (req, reply) => {
+      const label = new app.objection.models.label();
+      label.$set(req.body.data);
+
+      try {
+        const validLabel = await app.objection.models.label.fromJson(req.body.data);
+        await app.objection.models.label.query().insert(validLabel);
+        req.flash('info', i18next.t('flash.label.create.success'));
+        reply.redirect(app.reverse('root'));
+      } catch (err) {
+        req.flash('error', i18next.t('flash.label.create.error'));
+        reply.render('label/new', { label, errors: err.data });
+      }
+
+      return reply;
+    })
 };
