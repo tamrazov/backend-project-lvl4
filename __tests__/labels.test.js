d75c39e971c490b01b78e8399bd61ec319607233
Alex <box-570@yandex.ru>
2022-08-05 19:47:05 +0300

    add test

--- __tests__/labels.test.js
+++ __tests__/labels.test.js
@@ -1,6 +1,6 @@
 import fastify from 'fastify';
 import init from '../server/plugin.js';
-import { prepareData } from './helpers/index.js';
+import { getTestData, prepareData } from './helpers/index.js';
 
 // @ts-ignore
 
@@ -8,7 +8,7 @@ describe('test labels CRUD', () => {
   let app;
   let knex;
   // let models;
-  // const testData = getTestData();
+  const testData = getTestData();
 
   beforeAll(async () => {
     app = fastify();
@@ -44,4 +44,59 @@ describe('test labels CRUD', () => {
 
     expect(response.statusCode).toBe(200);
   });
+
+  test('edit page', async () => {
+    const response = await app.inject({
+      method: 'GET',
+      url: app.reverse('editLabel', { id: 1 }),
+    });
+
+    expect(response.statusCode).toBe(200);
+  });
+
+  test('create', async () => {
+    const params = testData.labels.new;
+    const response = await app.inject({
+      method: 'POST',
+      url: app.reverse('createLabel'),
+      payload: {
+        data: params,
+      },
+    });
+
+    expect(response.statusCode).toBe(200);
+    const label = await models.label.query().findOne({ name: params.name });
+    expect(label).toMatchObject(params);
+  });
+
+  test('delete', async () => {
+    const { id } = testData.labels.deleting;
+    const response = await app.inject({
+      method: 'DELETE',
+      url: app.reverse('deleteLabel', { id }),
+    });
+
+    expect(response.statusCode).toBe(200);
+
+    const label = await models.label.query().findById(id);
+    expect(label).toBeUndefined();
+  });
+
+  // test('edit', async () => {
+  //   const params = testData.statuses.editing;
+  //   const { id } = testData.statuses.existing2;
+
+  //   const response = await app.inject({
+  //     method: 'POST',
+  //     url: app.reverse('editStatus', { id }),
+  //     payload: {
+  //       data: params,
+  //     },
+  //   });
+
+  //   const status = await models.status.query().findById(id);
+
+  //   expect(response.statusCode).toBe(200);
+  //   expect(status.name).toBe(params.name);
+  // });
 });
